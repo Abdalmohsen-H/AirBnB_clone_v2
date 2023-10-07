cc62050fde80bbeb1f22607233b0cb5b9fcc8d54
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-06 20:19:07 -0700

    fix task 101

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,40 +3,29 @@ exec {'install_nginx':
   command  => 'sudo apt-get update ; sudo apt-get -y install nginx',
   provider => shell,
 }
-
 -> package {'nginx':
   ensure   => 'present',
   provider => 'apt'
 }
-
 -> exec {'create_multi_dirs':
   command  => 'sudo mkdir -p /data/web_static/releases/test/ ; sudo mkdir -p /data/web_static/shared/',
   provider => shell,
 }
-
 -> file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "this is just content for testing for web static with fabric project\n"
 }
-
 -> file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
 }
-
--> exec { 'change_permission':
-  command  => 'sudo chown -R ubuntu:ubuntu /data/',
-  provider => shell,
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
--> file_line { 'add_path_location_to_nginx_config':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  line   => "   server_name _;
-        location /hbnb_static {
-                alias /data/web_static/current/;
-        }",
-  match  => '^\tserver_name _;',
+-> exec {'replace_config_with_sed':
+  command  => 'sh -c \'sed -i "/server_name/a location /hbnb_static { alias /data/web_static/current/;}" /etc/nginx/sites-enabled/default\' ',
+  provider => shell,
 }
 
 -> exec {'apply_new_config':
