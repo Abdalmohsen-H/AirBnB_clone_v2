261ac63d633700a12fecafe7ebce7afa9300409d
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-21 10:18:31 -0700

    task 2:add another flask route

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Flask app demo for basic route
+just another hello world example
+with specifing IP and port
+and disabling strict_slashes on routes
+"""
+from flask import Flask
+
+# make an instance on Flask class and assign
+# it to app variable
+app = Flask(__name__)
+
+# disable strict _slashes globally
+# by default it was on
+app.url_map.strict_slashes = False
+# another way on @app.route decorator
+# @app.route('/', strict_slashes=False)
+
+
+@app.route("/")
+def hello_w():
+    ''' return string “Hello HBNB!”
+    on get request on "/" route
+    '''
+    return ("Hello HBNB!")
+
+@app.route("/hbnb")
+def hello_w():
+    ''' return string “Hello HBNB!”
+    on get request on "/" route
+    '''
+    return ("HBNB")
+
+if __name__ == "__main__":
+    # before below lines
+    # you still could run this using
+    # flask --app 0-hello_route run
+
+    # but here we will define host and port
+    # you also could do this on command line
+    # with --port --host options
+    app.run(host="0.0.0.0", port=5000, debug=False)
