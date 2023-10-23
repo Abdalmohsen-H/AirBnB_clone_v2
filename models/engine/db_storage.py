ea7fe51071009b4e1d8750e00c73734de157af64
Abdalmohsen-H <egywebdes@gmail.com>
2023-10-23 00:37:16 -0700

    fix task 7 : to creates a new Session after close()st

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -101,7 +101,7 @@ class DBStorage:
         session_factory = sessionmaker(
             bind=self.__engine, expire_on_commit=False
         )
-        self.__session = scoped_session(session_factory)
+        self.__session = scoped_session(session_factory)()
 
     def close(self):
         """
